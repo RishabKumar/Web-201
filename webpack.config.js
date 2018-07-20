9bc14c36271b002218aead0ec4373a35f4f1a8ab
Rishabh Kumar <riskumar3@publicisgroupe.net>
2018-07-20 16:25:01 +0530

    updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,10 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader'
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
                 }
             },
             {
